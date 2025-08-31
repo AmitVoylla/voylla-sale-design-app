6cb009e7ce9ce2023e35676c27d9a4908e7ef0e2
AmitVoylla <amit.singh@voylla.com>
2025-08-31 23:50:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -243,6 +243,7 @@ if "agent_executor" not in st.session_state:
         handle_parsing_errors=True,
         memory=st.session_state.memory,
         max_iterations=18,
+        early_stopping_method="generate",
         agent_executor_kwargs={"handle_parsing_errors": True}
     )
 
