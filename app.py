900a63ae44331b14b800ff8899ca043f6bc84764
AmitVoylla <amit.singh@voylla.com>
2025-11-10 13:25:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,9 @@
 # coding: utf-8
 import streamlit as st
 from langchain_openai import ChatOpenAI
-# from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationChain
-
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain.memory import ConversationBufferMemory
-
+from langchain.chains import ConversationChain
 from langchain.prompts import PromptTemplate
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
