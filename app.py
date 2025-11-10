664a451691d2b2d46625e5c006a3cf724e6c2288
AmitVoylla <amit.singh@voylla.com>
2025-11-10 13:18:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
-pip install -U langchain-core langchain-community
+# pip install -U langchain-core langchain-community
+import os
+os.system("pip install -U langchain-core langchain-community")
 
 
 import streamlit as st
